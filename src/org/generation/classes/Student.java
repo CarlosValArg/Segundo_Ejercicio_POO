385dc72469fc53ce4d52a7eb3a7e3c64721fef55
Carlos Valencia <cavalar1999@gmail.com>
2024-09-20 13:37:25 -0600

    Agregados archivos iniciales del ejercicio

--- src/org/generation/classes/Student.java
+++ src/org/generation/classes/Student.java
@@ -1,5 +1,23 @@
 package org.generation.classes;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Student
+{
+    private String name;
+
+    private String id;
+
+    private int age;
+
+    public final List<Course> courseList = new ArrayList<>();
+
+    public Student( String name, String id, int age)
+    {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
 }
--- src/org/generation/exceptions/CourseNotFoundException.java
+++ src/org/generation/exceptions/CourseNotFoundException.java
@@ -1,5 +1,10 @@
 package org.generation.exceptions;
 
-public class CourseNotFoundException {
+public class CourseNotFoundException extends Exception
+{
 
+    public CourseNotFoundException()
+    {
+        super("course not found!!");
+    }
 }
